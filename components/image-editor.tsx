f51e7702d62a6e5e282d1807eab2543626994d47
jakisundays <jacobguillermooo@gmail.com>
2024-09-12 23:47:32 -0500

    [Fix] useEffect BS error

--- components/image-editor.tsx
+++ components/image-editor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
